1c77046ce7fde8c955a7b3b86752fca71ed10985
Emma Rand <7593411+3mmaRand@users.noreply.github.com>
2023-11-20 10:45:24 +0000

    Initial commit

---
+++ process.R
@@ -0,0 +1,62 @@
+
+# DESCRIPTION -------------------------------------------------------------
+# The project contains 6 files of larvae tracking data generated from imageJ
+# The files are in the same format and contain the following columns:
+# Track, Length, Distance,	#Frames,	1stFrame,	time(s),	MaxSpeed	
+# AvgArea,	StdArea,	AvgPerim,	StdPerim,	AvgSpeed,	BLPS,	avgX,	avgX,	Bends,
+# BBPS
+
+# Note that avgX appears twice and the second will be renamed avgX_1 on import
+
+# The file names encode the larvae type (EV or 343) and the concentration of 
+# the treatment (P-coumpound)
+# 343_1.txt
+# 343_2.5.txt
+# 343_0.txt
+# ev_1.txt
+# ev_2.5.txt     
+# ev_0.txt
+
+
+# Packages ----------------------------------------------------------------
+library(tidyverse)
+
+
+
+# Import ------------------------------------------------------------------
+
+# import multiple csv files into one dataframe
+# using map_df() from purrr package
+# clean the column names up using janitor::clean_names()
+files <- list.files(path = "track", full.names = TRUE )
+tracking <- files |> 
+  set_names() |>
+  map_dfr(read_table, .id = "file") |>
+  janitor::clean_names()
+
+
+
+# Process the file name column --------------------------------------------
+
+# extract type and concentration from file name
+# and put them into additopnal separate columns
+tracking <- tracking |> 
+  mutate(file = str_remove(file, ".txt")) |>
+  mutate(file = str_remove(file, "track/")) |>
+  extract(file, remove = 
+            FALSE,
+          into = c("type", "conc"), 
+          regex = "([^_]{2,3})_(.+)") 
+
+# convert concentration to numeric
+tracking <- tracking |> 
+  mutate(conc = as.numeric(conc))
+
+
+# Plot --------------------------------------------------------------------
+
+
+# plot distance for each type and concentration
+tracking |> 
+  ggplot(aes(x = conc, y = max_speed, color = type)) +
+  geom_boxplot() 
